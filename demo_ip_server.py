6cb0d2a0b0e145cc86d307a1604a312b0b87a3f9
avivbh1 <avivben2004@gmail.com>
2023-10-24 18:27:59 +0300

    Completes demo server for handling SYN packets and ACK packets

--- demo_ip_server.py
+++ demo_ip_server.py
@@ -1,30 +1,20 @@
 from scapy.layers.inet import TCP, IP
-from scapy.sendrecv import send, sniff
+from scapy.sendrecv import sniff
 from datetime import datetime
+from ddos_manipulation.three_way_handshake_handlers import handle_syn, handle_ack
 
 
-def handle_syn(packet):
-    print(f"[{datetime.now()}] Received a SYN packet from {packet[IP].src}:{packet[TCP].sport}")
-
-    # Create and send a SYN-ACK response
-    syn_ack_segment = TCP(sport=12345, dport=packet[TCP].sport, flags="SA", seq=1, ack=packet[TCP].seq + 1)
-    response = IP(src=packet[IP].dst, dst=packet[IP].src) / syn_ack_segment
-    send(response)
-    print(f"[{datetime.now()}] Sent a SYN-ACK response to {packet[IP].src}:{packet[TCP].sport}")
-
-
-def handle_ack(packet):
-    if packet.haslayer(TCP) and packet[TCP].flags:
-        print(f"Received an ACK packet from {packet[IP].src}:{packet[TCP].sport}")
-
-
-def handle_three_way_handshake(packet):
-    if packet[TCP].flags.S == 1:
+def handle_three_way_handshake_server(packet):
+    if packet[TCP].flags.S == 1 and packet[TCP].flags.A == 0:
+        print(f"[{datetime.now()}] RECEIVED a SYN packet from {packet[IP].src}:{packet[TCP].sport}")
         handle_syn(packet)
-    if packet[TCP].flags.A == 1:
+        print(f"[{datetime.now()}] SENT a SYN-ACK response to {packet[IP].src}:{packet[TCP].sport}")
+
+    if packet[TCP].flags.S == 0 and packet[TCP].flags.A == 1:
+        print(f"[{datetime.now()}] RECEIVED an ACK packet from {packet[IP].src}:{packet[TCP].sport}")
         handle_ack(packet)
 
 
 def run_demo_server(port):
-    sniff(filter=f"tcp and port {port}", prn=handle_three_way_handshake)
+    sniff(filter=f"tcp and port {port}", prn=handle_three_way_handshake_server)
     print(f"demo server runs on PORT f{port}")
