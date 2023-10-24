b33e320846c5ae09720c4c058855bfa387e565a4
avivbh1 <avivben2004@gmail.com>
2023-10-24 23:38:26 +0300

    Adds TCP demo server using sockets

--- demo_ip_server.py
+++ demo_ip_server.py
@@ -1,20 +1,39 @@
-from scapy.layers.inet import TCP, IP
-from scapy.sendrecv import sniff
-from datetime import datetime
-from ddos_manipulation.three_way_handshake_handlers import handle_syn, handle_ack
+import socket
+
+server_ip = "0.0.0.0"
+server_port = 9900
 
 
-def handle_three_way_handshake_server(packet):
-    if packet[TCP].flags.S == 1 and packet[TCP].flags.A == 0:
-        print(f"[{datetime.now()}] RECEIVED a SYN packet from {packet[IP].src}:{packet[TCP].sport}")
-        handle_syn(packet)
-        print(f"[{datetime.now()}] SENT a SYN-ACK response to {packet[IP].src}:{packet[TCP].sport}")
+def handle_client(client_socket):
+    # Handle incoming packets from the client
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            print("Reached. No data sent")
+            break
+        print(f"Received data: {data.decode()}")
 
-    if packet[TCP].flags.S == 0 and packet[TCP].flags.A == 1:
-        print(f"[{datetime.now()}] RECEIVED an ACK packet from {packet[IP].src}:{packet[TCP].sport}")
-        handle_ack(packet)
+
+def main():
+    # Create a TCP socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the IP address and port
+    server_socket.bind((server_ip, server_port))
+
+    # Listen for incoming connections
+    server_socket.listen()
+
+    print(f"Listening on {server_ip}:{server_port}")
+
+    while True:
+        # Accept an incoming connection
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+
+        # Handle the client's packets
+        handle_client(client_socket)
 
 
-def run_demo_server(port):
-    sniff(filter=f"tcp and port {port}", prn=handle_three_way_handshake_server)
-    print(f"demo server runs on PORT f{port}")
+if __name__ == "__main__":
+    main()
