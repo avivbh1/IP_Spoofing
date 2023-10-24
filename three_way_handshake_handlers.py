d3c55006f3d5c72a223cbf9ee2b08e3c2419bbe5
avivbh1 <avivben2004@gmail.com>
2023-10-24 18:27:16 +0300

    Splitting the threeway handshake handlers

---
+++ three_way_handshake_handlers.py
@@ -0,0 +1,28 @@
+from scapy.layers.inet import TCP, IP
+from scapy.sendrecv import send
+from datetime import datetime
+
+
+def handle_syn(packet):
+    if packet.haslayer(TCP) and (packet[TCP].flags.S == 1 and packet[TCP].flags.A == 0): # reassurance of packet
+        # segment
+
+        # Create and send a SYN-ACK response
+        syn_ack_segment = TCP(sport=12345, dport=packet[TCP].sport, flags="SA", seq=1, ack=packet[TCP].seq + 1)
+        response = IP(src=packet[IP].dst, dst=packet[IP].src) / syn_ack_segment
+        send(response)
+
+
+def handle_syn_ack(packet):
+    if packet.haslayer(TCP) and (packet[TCP].flags.S == 0 and packet[TCP].flags.A == 1):  # reassurance of packet
+        # segment
+
+        # Create and send a SYN-ACK response
+        ack_segment = TCP(sport=12345, dport=packet[TCP].sport, flags="A", seq=1, ack=packet[TCP].seq + 1)
+        response = IP(src=packet[IP].dst, dst=packet[IP].src) / ack_segment
+        send(response)
+
+
+def handle_ack(packet):
+    if packet.haslayer(TCP) and (packet[TCP].flags.S == 0 and packet[TCP].flags.A == 1):
+        print(f"Received an ACK packet from {packet[IP].src}:{packet[TCP].sport}")
